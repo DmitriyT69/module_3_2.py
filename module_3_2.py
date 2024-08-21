6e6291612faf81f8ad2733ddf24425714664814b
DmitriyT69 <td2598793@gmail.com>
2024-08-21 18:08:36 +0400

    module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -3,7 +3,7 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
         print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
         print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
-    elif ("@" and (".com" or ".ru" or ".net")) not in recipient and ("@" and (".com" or ".ru" or ".net")) not in sender:
+    elif ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
         print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
     else:
         print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
